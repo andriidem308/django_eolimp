04a9a6ec21dacbc03bc7702bd0942930d3388065
olnikiforov <nikiforovsh@gmail.com>
2022-05-15 20:02:12 +0300

    background commit

--- django_eolimp/settings.py
+++ django_eolimp/settings.py
@@ -122,10 +122,10 @@ USE_TZ = True
 # Static files_uploaded (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'testing/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'testing/static'),
 )
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
