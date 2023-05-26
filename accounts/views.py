26c975cba686ab15a77cf6a106378ce9275f7a89
Andriy Demchenko <a.demchenko@gethatch.com>
2023-05-26 23:54:15 +0300

    updated login

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,6 @@ from django.contrib.auth import views
 from django.urls import path
 
 urlpatterns = [
-    path('login/', views.LoginView.as_view(), name='login'),
+    # path('login/', views.LoginView.as_view(), name='login'),
 ]
 # Create your views here.
