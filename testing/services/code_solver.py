db6842220157a63cc1fa86c6e2abc82b7a6b2d0c
Andriy Demchenko <a.demchenko@gethatch.com>
2023-05-29 21:46:14 +0300

    added fakers

--- testing/services/code_solver.py
+++ testing/services/code_solver.py
@@ -1,48 +1,38 @@
+import json
 import os
 import time
 from subprocess import PIPE, Popen
 
 
-SEPARATOR = '!'
+def test_student_solution(code, exec_time, test_filename):
+    tests = json.load(open(test_filename))
+    temporary_filename = 'test_solution.py'
+    with open(temporary_filename, 'w') as temporary_file:
+        temporary_file.write(code)
 
+    successful_tests = 0
+    total_tests = len(tests)
 
-def test_student_solution(code, exec_time, file_in, file_out):
-    temporary_file = 'test_solution.py'
-    open(temporary_file, 'w').write(code)
+    for test in tests:
+        test_inputs = test.get('inputs', [])
+        test_outputs = test.get('outputs', [])
 
-    try:
-        test_outputs = list(map(lambda x: x.strip(), open(file_out.path, 'r').readlines()))
-        test_inputs = list(map(lambda x: x.strip(), open(file_in.path, 'r').readlines()))
+        command = [f'python3 {temporary_filename}']
 
-        code_outputs = []
+        start_time = time.time()
+        p = Popen(command, stdin=PIPE, stdout=PIPE, shell=True)
+        shell_input = bytes('\n'.join(test_inputs), encoding='utf-8')
+        buffer, buffer_err = p.communicate(input=shell_input)
 
-        for curr_test_inputs in test_inputs:
-            inputs_list = curr_test_inputs.split(SEPARATOR)
+        end_time = time.time()
 
-            command = [f'python3 {temporary_file}']
+        if (end_time - start_time) * 1000 <= exec_time:
+            result_outputs = buffer.rstrip().decode('utf-8').split('\n')
+            if result_outputs == test_outputs:
+                successful_tests += 1
 
-            start_time = time.time()
-            p = Popen(command, stdin=PIPE, stdout=PIPE, shell=True)
-            buffer = p.communicate(input=bytes('\n'.join(inputs_list), encoding='utf-8'))[0]
-            end_time = time.time()
+    score = successful_tests / total_tests
 
-            if (end_time - start_time) * 1000 > exec_time:
-                code_outputs.append(None)
-            else:
-                code_outputs.append(buffer.rstrip().decode('utf-8').split('\n'))
+    os.remove(temporary_filename)
 
-        test_success = 0
-        test_total = len(test_outputs)
-
-        for code_output, test_output in zip(code_outputs, test_outputs):
-            if code_output:
-                if code_output[0] == test_output:
-                    test_success += 1
-
-        score = test_success / test_total
-    except Exception as e:
-        # If Student's code return some exception, his score of this problem becomes equal to zero
-        score = 0
-
-    os.remove(temporary_file)
     return score
