02357fe2f86950bda3ac763d3e5e9c0062c81045
olnikiforov <nikiforovsh@gmail.com>
2023-10-01 19:57:13 +0300

    add verification.py

---
+++ testing/services/verification.py
@@ -0,0 +1,26 @@
+import random
+import string
+
+from django.core.mail import send_mail
+
+from django_eolimp.settings import EMAIL_HOST_USER
+
+def send_verification_passcode(email):
+    passcode = generate_passcode()
+
+    subject = 'Verify your account'
+    message = "Your passcode for your account: " + passcode
+    print(f'EMAIL:{email}')
+    print(f'EMAILhost:{EMAIL_HOST_USER}')
+    send_mail(subject, message, EMAIL_HOST_USER, [email])
+
+    return passcode
+
+
+def generate_passcode():
+    length = 6
+    characters = string.ascii_letters + string.digits
+
+    random_passcode = ''.join(random.choice(characters) for _ in range(length))
+
+    return random_passcode
